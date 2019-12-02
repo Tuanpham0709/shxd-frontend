33f9d5023d23833b29d793ff705c4077a71d4e5d
Pham Truc <trucdev@Phams-MacBook-Pro.local>
2019-12-02 13:14:25 +0700

    ....

--- src/graphql/photo/uploadPhoto.ts
+++ src/graphql/photo/uploadPhoto.ts
@@ -1,9 +1,9 @@
-import { gql }  from 'apollo-boost';
+// import { gql }  from 'apollo-boost';
 
-export const UPLOAD_PHOTO = gql`
-mutation UploadPhoto($file: Upload!, $dimensions: PhotoDimensionsInput!) {
-  uploadPhoto(file: $file, dimensions: $dimensions) {
-    _id
-  }
-}
-`;
+// export const UPLOAD_PHOTO = gql`
+// mutation UploadPhoto($file: Upload!, $dimensions: PhotoDimensionsInput!) {
+//   uploadPhoto(file: $file, dimensions: $dimensions) {
+//     _id
+//   }
+// }
+// `;
