6b4dcd8708bddb2f047945e7a60d15f5ecc1c342
Tuấn Phạm <om.phamtuan1014@gmail.com>
2019-12-06 08:46:21 +0700

    UI Feature Project Management

--- config-overrides.js
+++ config-overrides.js
@@ -7,7 +7,7 @@ module.exports = override(
     modifyVars: {
       'primary-color': '#37b5a3',
       'link-color': '#37b5a3',
-      'input-height-base': '48px',
+      'input-height-base': '35px',
       'btn-height-base': '48px',
       'border-radius-base': '3px',
       'font-size-base': '14px',
